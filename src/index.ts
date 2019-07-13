58c54c9fec894119de3a65f03ca6d8d4477ca440
Shahaed Hasan <shahaed77@gmail.com>
2019-07-13 03:12:15 -0500

    :sparkles: Added Firestore database client

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import { Client } from "discord.js";
 import * as dotenv from "dotenv";
-import { presence_update } from "./presence_update";
+import { FirestoreDB } from "./database/firestore";
+import { PlayerServices } from "./services/player_services";
 
 /**
  * Entry point that spawns the Discord client
  */
-dotenv.config();
-const client = new Client();
-
+dotenv.config({ path: "./secrets/.env" });
+const client: Client = new Client();
 client.login(process.env.BOT_TOKEN);
+
+const db = new FirestoreDB(process.env.FIRESTORE_projectId, process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const player_service = new PlayerServices(db);
 
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
+    player_service.get_player_by_id("344614286368440330").then(data => {
+        console.log(data);
+    });
 
-presence_update(client);
+    client.fetchUser("344614286368440330").then(user => {
+        player_service.add_player(user);
+    });
+});
---
+++ src/services/text_channel.ts
@@ -0,0 +1,13 @@
+import { Client } from "discord.js";
+
+export class TextChannel {
+  client: Client;
+
+  constructor(client: Client) {
+    this.client = new Client;
+  }
+
+  send_to_channel(channel: string, message: string) {
+    return;
+  }
+}

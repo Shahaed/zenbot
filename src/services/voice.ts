2d8bf260b5cd793a128adcc3f79e8259842a2514
Paul Ruales <paulruales1@gmail.com>
2019-07-13 01:35:50 -0500

    :sparkles: Added voice functionality
    Ending the timer with a youtube video audio played on the users voice channel.

--- src/services/voice.ts
+++ src/services/voice.ts
@@ -1,3 +1,24 @@
-class Voice {
+import { GuildMember } from "discord.js";
+import ytdl from "ytdl-core";
 
+const audioStream: string = "https://youtu.be/EHmC2D0_Hdg";
+
+export class Voice {
+
+    static async endTimerInVoiceChannel(member: GuildMember) {
+        if (!this.voiceChannelAvailable(member)) {
+            return "false";
+        }
+        const connection = await member.voiceChannel.join();
+        const stream = ytdl(audioStream, { filter: "audioonly" });
+
+        console.log(stream);
+        const dispatcher = connection.playStream(stream);
+
+        return dispatcher;
+    }
+
+    static voiceChannelAvailable(member: GuildMember) {
+        return member.voiceChannel != undefined;
+    }
 }
